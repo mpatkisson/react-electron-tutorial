3f1fde3652f114bdb196c0419bbfa2391e10ce7e
Mike Atkisson <mpatkisson@gmail.com>
2015-12-19 04:59:52 -0500

    Part 3 - Transpiling with webpack (works)

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: './entry.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query:{ presets:['react'] }
+      }
+    ]
+  }
+};
