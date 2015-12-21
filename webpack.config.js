87651423718b5e75baa6b3a000d4e2a450f0a902
Mike Atkisson <mpatkisson@gmail.com>
2015-12-20 22:43:25 -0500

    Add publicPath to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
 
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/build'
+    path: __dirname + '/build',
+    publicPath: 'http://localhost:2016/build/'
   },
 
   module: {
