d6e592952c4c08ed7afad4455e332be7b9b7846c
Mike Atkisson <mpatkisson@gmail.com>
2015-12-21 03:37:46 -0500

    Part 5 - Configures SASS support for webpack

--- src/entry.js
+++ src/entry.js
@@ -1,12 +1,14 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 
+require('../static/styles/main.scss');
+
 var App = React.createClass({
   render: function() {
     var nameStyle = {
       color: 'red'
     };
-    return <h1>Hello, <span style={nameStyle}>World</span>, from React!</h1>;
+    return <h1 className="test">Hello, <span style={nameStyle}>World</span>, from React!</h1>;
   }
 });
 
